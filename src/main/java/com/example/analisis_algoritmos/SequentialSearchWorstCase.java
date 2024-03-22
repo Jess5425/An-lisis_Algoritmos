24e8bacfe2d2fce5017967e01c2e11b0ac116bf8
Jéssica Jiménez Ureña <jesjimenez@estudiantec.cr>
2024-03-21 20:20:55 -0600

    Código documentado.

--- src/main/java/com/example/analisis_algoritmos/SequentialSearchWorstCase.java
+++ src/main/java/com/example/analisis_algoritmos/SequentialSearchWorstCase.java
@@ -2,23 +2,24 @@ package com.example.analisis_algoritmos;
 
 import java.util.Random;
 
-public class SequentialSearchWorstCase {
+public class SequentialSearchWorstCase { //Hace la búsqueda secuencial.
 
     public static void main(String[] args) {
-        int arraySize = 10000;
-        int searchValue = 10001;
-        int[] array = generateRandomArray(arraySize, searchValue);
+        int arraySize = 65000;  //Este es el valor que se cambia para probar la duración con diferentes tamaños.
+        int searchValue = 55; //Este es el valor que se va a buscar.
+        int[] array = generateRandomArray(arraySize, searchValue); //Genera el array random.
 
         long startTime = System.nanoTime();
-        int searchIndex = sequentialSearch(array, searchValue);
+        int searchIndex = sequentialSearch(array, searchValue); //Esta variable guarda la posición del valor si existe.
         long endTime = System.nanoTime();
 
-        System.out.println("Array size: " + arraySize);
-        System.out.println("Search value: " + searchValue);
-        System.out.println("Search time: " + (endTime - startTime) + " nanoseconds");
-        System.out.println("Search result: " + (searchIndex != -1 ? "Found" : "Not found"));
+        System.out.println("Tamaño del array: " + arraySize);
+        System.out.println("Valor buscado: " + searchValue);
+        System.out.println("Tiempo de búsqueda: " + (endTime - startTime) + " nanoseconds");
+        System.out.println("Resultado: " + (searchIndex != -1 ? "Found" : "Not found"));
     }
 
+    //Array random
     public static int[] generateRandomArray(int arraySize, int searchValue) {
         int[] array = new int[arraySize];
         Random random = new Random();
@@ -30,6 +31,7 @@ public class SequentialSearchWorstCase {
         return array;
     }
 
+    //Búsqueda secuencial
     public static int sequentialSearch(int[] array, int searchValue) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == searchValue) {
